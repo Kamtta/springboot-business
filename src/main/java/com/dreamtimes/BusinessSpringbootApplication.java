3e371d4652036763d36cd6d38cc87c7b7ecf7016
chenjingrong <15822869305@163.com>
2019-01-10 20:41:20 +0800

    2019-01-10

---
+++ src/main/java/com/dreamtimes/BusinessSpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.dreamtimes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessSpringbootApplication.class, args);
+    }
+
+}
+
